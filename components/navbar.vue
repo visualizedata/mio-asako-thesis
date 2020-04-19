0bfaec20d6963418768b04ee90375d6e96ac88a5
miopio <mioakasako@gmail.com>
2020-04-19 16:24:00 -0400

    added BASE_URL

--- components/navbar.vue
+++ components/navbar.vue
@@ -2,9 +2,9 @@
     <div>
         <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
             <el-menu-item index="1"><NuxtLink to="/">HOME</NuxtLink></el-menu-item>
-            <el-menu-item index="2"><NuxtLink to="/misconduct">MISCONDUCT CASES</NuxtLink></el-menu-item>
-            <el-menu-item index="3"><NuxtLink to="/misconduct">NETWORKS</NuxtLink></el-menu-item>
-            <el-menu-item index="4"><NuxtLink to="/misconduct">PAPER TRAILS</NuxtLink></el-menu-item>
+            <el-menu-item index="2"><NuxtLink to="misconduct">MISCONDUCT CASES</NuxtLink></el-menu-item>
+            <el-menu-item index="3"><NuxtLink to="misconduct">NETWORKS</NuxtLink></el-menu-item>
+            <el-menu-item index="4"><NuxtLink to="misconduct">PAPER TRAILS</NuxtLink></el-menu-item>
         </el-menu>
         <div class="line"></div>
     </div>
