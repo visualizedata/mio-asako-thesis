c9bcc29ea5ea56a1441ad88ae9c7df6b34aedc62
miopio <mioakasako@gmail.com>
2020-04-23 19:08:29 -0400

    Turned circles into rectangles

--- components/asmd.vue
+++ components/asmd.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <h2>ASMD test</h2>
-
-     <svg ref="asmdCircleGraphSVG">
-
-    <!-- <ul>
-      <li v-for="(asmd, index) in asmdData" :key="index">{{asmd.Person}} ({{asmd.Outcome}})</li>
-    </ul> -->
-   
+    <svg ref="asmdCircleGraphSVG" v-bind:width= "width" :height = "height">
+      <!--<text v-for="(d,i) in asmdData">{{d["Person"]}}</text>-->
     </svg>
+    <div class = "cases">
+        <!--<h2 v-for="(d,i) in asmdData">Name: {{d["Person"]}}</h2>-->
+        <h2>Institution:</h2>
+        <h2>Discipline:</h2>
+        <h2>Story: </h2>
+
+    </div>
+
   </div>
 </template>
 
@@ -24,7 +27,7 @@ export default {
     return{
       chartTitle: "Academic Sexual Misconduct Cases",
       height: 800, //why does window.innerHeight not work?
-      width: 1200,
+      width: 1500,
       margin: {top: 25, left: 25, bottom: 25, right: 25 },
       //data: this.asmdData,
     }
@@ -74,7 +77,7 @@ export default {
       .filter(isValid);
 
     var incidentColor = function(e){
-      return e.toLowerCase().indexOf("resigned") === -1 ? "#0000ff" : "#ff0000";
+      return e.toLowerCase().indexOf("resigned") === -1 ? "#ff6767" : "#6767ff";
     }
 
     // we need this to convert a date of the form 2000 to 2000-01-01:12:00:000
@@ -112,6 +115,9 @@ export default {
       return a.length;
     }));
 
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
     var y = d3.scaleLinear()
         .range([this.height, 0])
         .domain([0, maxCases]); // FIXME: it almost works when I multiply by (this.width/this.height)
@@ -121,11 +127,13 @@ export default {
         .attr("width", this.width)
         .attr("height", this.height)
 
-      // add the tooltip area to the webpage
-      // var tooltip = d3.select(this.$refs.asmdCircleGraphSVG).append("div")
-      //     .attr("class", "tooltip")
-      //     .style("opacity", 0)
-      //     .style("fill", "ffffff");
+    console.log("window width: " + window.innerWidth);
+
+    // add the tooltip area to the webpage
+    var tooltip = d3.select(this.$refs.asmdCircleGraphSVG).append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 1)
+  
 
     // g container for each bin
     let binContainer = svg.selectAll(".gBin")
@@ -139,42 +147,42 @@ export default {
           // .attr("transform", d => `translate(${x(d.x0)}, ${this.height})`)
 
     // need to populate the bin containers with data the first time
-    binContainerEnter.selectAll("circle")
+    binContainerEnter.selectAll("rect")
           .data(d => d.map((p, i) => {
             return {idx: i,
                     name: p['Person'],
                     outcome: p.Outcome,
                     institution: p["Institution"],
                     discipline: p["Specific Discipline"],
-                    link: p["Link(s)"],
-                    radius: circleRadius,
-                    color: "#0000ff"
+                    link: p["Original Link(s)"],
+                    color: "#ffffff"
                   }
           }))
         .enter()
-        .append("circle")
+        .append("rect")
           .attr("class", "enter")
-          .attr("cx", 0) // g element already at correct x pos
-          .attr("cy", function(d) {
+          .attr("x", 0) // g element already at correct x pos
+          .attr("y", function(d) {
                 return - 2*circleRadius * ( d.idx + 0.5 );
                })
-          .attr("r", circleRadius)
+          .attr("height", circleRadius)
+          .attr("width", circleRadius *3)
           .style("fill", function(d){ return incidentColor(d.outcome); })
 
-          //.on("mouseover", function(d, i){console.log(value[i])})
-          //.on("mouseover", tooltipOn)
-          //.on("click", tooltipOff)
-          //.on("mouseout", tooltipOff)
-          //.on("click", function(d){
-          //  window.open(d.link)
-          //})
+          .on("mouseover", function(d, i){console.log("Mousover outcome:"  + outcome[i])})
+          .on("mouseover", tooltipOn)
+          .on("click", tooltipOff)
+          .on("mouseout", tooltipOff)
+          .on("click", function(d){
+           window.open(d.link)
+          })
 
       binContainerEnter.merge(binContainer)
           .attr("transform", d => `translate(${x(d.x0)}, ${this.height})`);
       
       // add x axis
       svg.append("g")
-        .style("font", "12px futura-pt")
+        .style("font", "12px helvetica")
         .attr("class", "axis axis--x")
         .attr("transform", "translate(0," + this.height + ")")
         .style("stroke", "white")
@@ -182,14 +190,68 @@ export default {
 
       // add y axis
       svg.append("g")
-        .style("font", "12px futura-pt")
+        .style("font", "12px helvetica")
         .attr("class", "axis axis--y")
         .style("stroke", "white")
         .call(d3.axisRight(y));
+      
+      function tooltipOn(d) {
+        // x position of parent g element
+        let gParent = d3.select(this.parentElement)
+        let translateValue = gParent.attr("transform")
+
+        d3.select(this)
+          .classed("selected", true)
+          .style("opacity", .5)
+          .style("cursor", "pointer")
+        tooltip.transition()
+            .duration(200)
+            .style("opacity", .9);
+        tooltip.html("<b><span style = 'font-size: 20px; color: #bf033b; text-transform: uppercase;'>"+ d.name + "</span></b>" + "</br>" + "<b>Outcome:  </b>" + d.outcome + "</br>" + "<b>Institution:  </b>" + d.institution + "</br>" +  "<b>Discipline:  </b>" + d.discipline + "</br>" + "<a href= '" + d.link + "''>" + "</a>")
+          .style("left", 300 + "px")
+          .style("top", 300 + "px")
+          .style("margin", "10px");
+    }
+
+    function tooltipOff(d) {
+      d3.select(this)
+          .classed("selected", false)
+          .style("fill", function(d){ return d.color; })
+          .style("opacity", 1)
+        tooltip.transition()
+            .duration(500)
+            .style("opacity", 1);
+    }
 
   },
   watch: {
-    }
+  }
 };
 </script>
+
+<style scoped>
+
+.tooltip {
+  color: white;
+  position: relative;
+  text-align: left;
+  /*text-transform: uppercase;*/
+  max-width: 400px;
+  height: auto;
+  font-family: helvetica, sans-serif;
+  font-size: 15px;
+  background: #ffffff;
+  display: inline-block;
+  border: 1px solid #777777;
+  /*border-radius: 5px;*/
+  pointer-events: none;
+  box-shadow: 0 6px 12px rgba(0,0,0,.5);
+  margin: 5px;
+  padding: 10px;
+}
+
+.cases {
+  border: 5px solid #ff0000;
+}
+</style>
 
--- pages/misconduct.vue
+++ pages/misconduct.vue
@@ -1,11 +1,11 @@
 <template>
   <el-row>
-    <el-col :span="0"><p>Misconduct component</p></el-col>
-    <el-col :span="24">
+    <el-col :span="2"><p>Misconduct component</p></el-col>
+    <el-col :span="20">
       <p>Misconduct</p>
       <asmd :asmdData=loadedAsmdData />
     </el-col>
-    <el-col :span="0"><h1>Misconduct again</h1></el-col>
+    <el-col :span="2"><p>Misconduct again</p></el-col>
   </el-row>
 
 </template>
