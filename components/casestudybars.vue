0b0da592d2cb3a25b474f36cd5ee5e42486a4620
miopio <mioakasako@gmail.com>
2020-05-01 23:43:53 -0400

    Bars look nice

--- components/casestudybars.vue
+++ components/casestudybars.vue
@@ -51,85 +51,89 @@ export default {
 
     var parseDate = d3.timeParse("%Y");
 
-    // var x = d3.scaleTime()
-    //     .domain([new Date("1970-01-01"), new Date("2019-12-31")])
-    //     .rangeRound([[this.margin.left, this.width-this.margin.right]]);
-    
+    const midYear = 1995;
+
+    // our time scale is centered around 1995, with 25 years left and right of it
     var x = d3.scaleTime()
-        .rangeRound([0,this.width])
-        .domain([new Date(1970, 1, 1), new Date(2019, 12, 31)])
+              .rangeRound([0,this.width])
+              .domain([new Date(1970, 1, 1), new Date(2020, 1, 1)])
 
-    console.log("check timeparse:")
-    console.log(parseDate(2018))
-    console.log(x(parseDate(2018)))
+    // create a custom x-axis for our timescale
+    // it is 50 years wide, centered around the year 0
+    var x2 = d3.scaleLinear()
+            .rangeRound([this.margin.left, this.width-this.margin.right])
+            .domain([-25, 25])
 
-    // var x = d3.scaleLinear()
-    //         .domain(d3.extent(caseStudyData, d => d.x2))
-    //         .rangeRound([this.margin.left, this.width-this.margin.right])
+    // one tick every 5 years
+    var xAxis = d3.axisTop(x2)
+                  .ticks(10)
 
+    // divide y-axis by number of cases
     var y = d3.scaleBand()
-            .domain(d3.range(caseStudyData.length))
-            .rangeRound([this.margin.top, this.height-this.margin.bottom])
-            .padding(0.1)
-    
-    
-    //make sure data is going through x and y variables
-    console.log("x and y domains:")
-    console.log(d3.range(caseStudyData.length));
-    console.log(d3.extent(caseStudyData, d => d.x2))
+              .domain(d3.range(caseStudyData.length))
+              .rangeRound([this.margin.top, this.height-this.margin.bottom])
+              .padding(0.1)
 
+    // we calculate a year in pixels on the x-axis to be able to translate
+    // back to year 0
+    const yearInPixels = x(parseDate(2001)) - x(parseDate(2000));
 
-    var testData = [ {title: "test", value: 123}, {title:"asasd", value: 456} ];
-
+    // draw on our SVG
     var svg = d3.select(this.$refs.caseStudiesSVG)
         .attr("width", this.width)
         .attr("height", this.height)
         .attr("viewBox", [0,0,this.width,this.height])
-        
-        
+    
+    // draw a rectangle for each case, then translate it to the mid-year
     svg.append("g")
         .selectAll("rect")
         .data(caseStudyData)
         .join("rect")
             .attr("fill", "#6767ff")
-            //.attr("x", x(0))
             .attr("x", d => x(parseDate(d.first_incident)))
             .attr("y", (d,i) => y(i))
-            .attr("width", d => x(parseDate(d.outcome)))
-            .attr("height", y.bandwidth());
+            // width is number of years between outcome and first_incident (times yearInPixels)
+            .attr("width", d => yearInPixels * (d.outcome - d.first_incident))
+            .attr("height", y.bandwidth())
+            // translate the rectangle to that first_complaint is in the middle
+            .attr("transform", d => `translate(${yearInPixels * (midYear - d.first_complaint)}, ${0})`)
     
+    // draw the name of the incident
     svg.append("g")
         .attr("fill", "white")
         .attr("text-anchor", "end")
         .attr("font-family", "Lato")
         .attr("font-size", 12)
         .selectAll("text")
-    .data(caseStudyData)
-    .join("text")
-        .attr("x", d => x(parseDate(d.first_incident)) - 4)
-        .attr("y", (d, i) => y(i) + y.bandwidth() / 2)
-        .attr("dy", "0.35em")
-        .text(d => d.name);
+        .data(caseStudyData)
+        .join("text")
+            .attr("x", d => x(parseDate(d.first_incident)) - 4)
+            .attr("y", (d, i) => y(i) + y.bandwidth() / 2)
+            .attr("dy", "0.35em")
+            .text(d => d.name)
+            // move the text since we moved the box
+            .attr("transform", d => `translate(${yearInPixels * (midYear - d.first_complaint)}, ${0})`)
 
+    // append the x-axis
     svg.append("g")
         .style("font", "12px helvetica")
         .attr("class", "axis axis--x")
         .attr("transform", "translate(0," + this.height + ")")
         .style("stroke", "white")
-        .call(d3.axisTop(x));
+        .call(xAxis)
 
+    // draw the first_complaint at midYear
     var bars = svg.selectAll(".bar")
         .data(caseStudyData)
-        .enter();
-
-    bars.append("rect")
-        .attr("class", "bar")
-        .attr("x", d => x(parseDate(d.first_complaint)))
-        .attr("y", (d,i) => y(i))
-        .attr("width", 2)
-        .attr("height", y.bandwidth())
-        .attr("fill", "#ffffff")
-    
+        .enter()
+        .append("rect")
+            .attr("class", "bar")
+            .attr("x", d => x(parseDate(d.first_complaint)))
+            .attr("y", (d,i) => y(i))
+            .attr("width", 2)
+            .attr("height", y.bandwidth())
+            .attr("fill", "#ffffff")
+            .attr("transform", d => `translate(${yearInPixels * (midYear - d.first_complaint)}, ${0})`)
 
 },
   watch: {
