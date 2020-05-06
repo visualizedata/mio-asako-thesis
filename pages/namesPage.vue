9a060219bb2a897db3e903a5ae5dee3df35bafc8
miopio <mioakasako@gmail.com>
2020-05-06 15:34:08 -0400

    taking out namepage files, hopefully fixes crashes?

--- pages/namesPage.vue
+++ pages/namesPage.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
+    <p>placeholder</p>
   </div>
 </template>
 
 <script>
-import names from "~/static/womeninSTEM.txt";
+
 
 export default {
   data() {
@@ -16,31 +17,7 @@ export default {
   },methods: {
   },
   mounted(){
-    const names = this.names;
-
-    const script = function (p5) {    
-      // NOTE: Set up is here   
-      p5.setup = _ => {      
-      p5.createCanvas(p5.windowWidth, p5.windowHeight);      
-      p5.background(86); 
-      //var names = p5.loadStrings(names, processData)
-      }
-
-      function processData(){
-        console.log(names);
-      }
-
-
-
-      // NOTE: Draw is here
-      p5.draw = _ => {      
-  
-      }  
-    }   
-    // NOTE: Use p5 as an instance mode
-    const P5 = require('p5');
-    new P5(script)
-  }
+}
 };
 </script>
 
