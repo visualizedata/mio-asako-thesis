b86155a7e7ab4bc51a1e072bd3dcc8c75646d13f
miopio <mioakasako@gmail.com>
2020-05-07 20:54:17 -0400

    readme

--- pages/index.vue
+++ pages/index.vue
@@ -12,9 +12,6 @@
           <p id = "author">by Mio Akasako | Parsons School of Design MS Data Visualization | May 2020</p>
         </el-col>
       </el-row>
-      <el-row>
-        <el-button class="el-button-home" round icon = "el-icon-arrow-down" v-scroll-to = "{ el: '#intro'}"></el-button>
-      </el-row>
     </div>
     <el-row id = "intro" :style= "{backgroundImage: 'url(' + images.tarana + ')' }">
       <el-col :span="8">
@@ -33,7 +30,6 @@
           for other humans, and yet time and time again those who engage in it have been told it is , by virtue of inaction, 
           or lenient sentences. Why is this so?</p>
           </div>
-          <el-button class="el-button-home" round icon = "el-icon-arrow-down" v-scroll-to = "{ el: '#consent'}"></el-button>
       </el-col>
       <el-col :span="8">
       </el-col>
@@ -52,7 +48,6 @@
             of adults age 18-95 across the US were asked questions about their views on consent and sexual assault.</p>
             <p>Women overall showed a better understanding of the definition of consent, regardless of age, marital status, 
             age, and ethnicity. However, people still held varying perspectives on what actions constituted sexual consent.</p>
-            <el-button class="el-button-home" round icon = "el-icon-arrow-down" v-scroll-to = "{ el: '#assault'}"></el-button>
         </el-col>
       </el-row>
     </div>
@@ -65,7 +60,6 @@
           on these matters. Most reported that they did not get education about consent and sexual assault in middle or high 
           school, and that their parents did not talk to them about these issues when growing up. </p>
         </div>
-        <el-button class="el-button-home" round icon = "el-icon-arrow-down" v-scroll-to = "{ el: '#stem_intro'}"></el-button>
       </el-col>
       <el-col :span="12">
         <img :src = "images.assault" style = "max-width: 100%">
@@ -81,7 +75,6 @@
             outside of the military.  In STEM academia, though the effect of the #MeToo movement have been observed, more 
             must be done to provide a fair, equal, and respectful environment for women in the field.</p>
           </div>
-          <el-button class="el-button-home" round icon = "el-icon-arrow-down" v-scroll-to = "{ el: '#stem_intro_2'}"></el-button>
       </el-col>
       <el-col :span="8">
       </el-col>
@@ -97,7 +90,7 @@
             academia as a career. Here you can explore the breakdown of sexual misconduct cases on a macro and micro scale, 
             and also celebrate the women who have persevered through this to establish their careers.</p>
           </div>
-          <el-button class="el-button-home"><NuxtLink to="chronologyPage">Take a look<i class="el-icon-arrow-right el-icon-right"></i></NuxtLink></el-button>
+          <el-button type = "info" class="el-button-home"><NuxtLink to="chronologyPage">Take a look<i class="el-icon-arrow-right el-icon-right"></i></NuxtLink></el-button>
       </el-col>
       <el-col :span="8">
       </el-col>
@@ -178,8 +171,16 @@ export default {
 
 .el-button-home{
   position:absolute;
-  bottom: 5vh;
-  left: 50%
+  left: 45%;
+  margin-bottom: 40vh;
+  background-color: #333333;
+  color: #ffffff;
+  text-decoration: none;
+  font-family: "Lato";
+}
+
+.a{
+  color: #ffffff;
 }
 
 #cover{
--- pages/namesPage.vue
+++ pages/namesPage.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
     <p>placeholder</p>
-    <el-steps :active="active" finish-status="success">
-      <el-step title="Step 1"></el-step>
-      <el-step title="Step 2"></el-step>
-      <el-step title="Step 3"></el-step>
     </el-steps>
   </div>
 </template>
